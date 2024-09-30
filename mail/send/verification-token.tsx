a6405f824337e5a86590881fcf5d19dc5c263c1c
Shema Elisa <shemaelyssa@gmail.com>
2024-09-30 12:03:30 +0200

    profile page

---
+++ mail/send/verification-token.tsx
@@ -0,0 +1,14 @@
+/**
+ * send Verification toke
+ * This function sends an email and takes the following parameters
+ * @param to {string} The receiver's email address
+ * @param subject {string} The email subject
+ * @param text {string} The email text
+ * @param verificationToken {string} The verification token
+ * @param username {string} The username
+ * @return {MailStatus} Mail Status
+ */
+export async function sendVerificationToken(
+  username: string,
+  verificationToken: string,
+) {}
